a7668b5e45c0c1cb1854c87a7dd4dd4bccb54025
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-12-03 21:03:15 +0530

    Add files via upload

---
+++ 1) Linked Lists/C/count_nodes_of_linkedlist/linkedlist_count3.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void Insert(struct node **head, int x) // **head is pointer to head
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = *head;
+    *head = temp;
+
+}
+
+void Print(struct node *head)
+{
+    struct node *temp = head;
+    printf("List is : ");
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void count_of_nodes(struct node *head)
+{
+    int count = 0;
+    if(head == NULL)
+    {
+        printf("Linked List is empty");
+    }
+    struct node *ptr = NULL;
+    ptr = head;
+    while(ptr != NULL)
+    {
+        count++;
+        ptr = ptr->next;
+    }
+    printf("\nThe Number of nodes is %d", count);
+}
+int main()
+{
+    struct node *head;
+    head = NULL;
+    printf("How many numbers?\n");
+    int i, n, x;
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter the number \n");
+        scanf("%d", &x);
+        Insert(&head, x);
+        Print(head);
+    }
+    count_of_nodes(head);
+
+}
+
+
+/*
+
+How many numbers?
+5
+Enter the number
+1
+List is : 1
+Enter the number
+2
+List is : 2 1
+Enter the number
+3
+List is : 3 2 1
+Enter the number
+4
+List is : 4 3 2 1
+Enter the number
+5
+List is : 5 4 3 2 1
+
+The Number of nodes is 5
+*/
