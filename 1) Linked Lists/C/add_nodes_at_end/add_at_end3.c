25177beadb8615d65fa198f43e0dd39ae18eb079
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-12-04 12:40:41 +0530

    Add files via upload

---
+++ 1) Linked Lists/C/add_nodes_at_end/add_at_end3.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void Insert(struct node **head, int x) // **head is pointer to head
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = *head;
+    *head = temp;
+
+}
+
+void Print(struct node *head)
+{
+    struct node *temp = head;
+    printf("List is : ");
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void add_at_end(struct node **head, int data)
+{
+    struct node *ptr, *temp;
+    ptr = *head;
+    temp = (struct node*)malloc(sizeof(struct node));
+
+    temp->data = data;
+    temp->next = NULL;
+
+    while(ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+}
+int main()
+{
+    struct node *head;
+    head = NULL;
+    printf("How many numbers?\n");
+    int i, n, x;
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter the number \n");
+        scanf("%d", &x);
+        Insert(&head, x);
+        Print(head);
+    }
+    add_at_end(&head, 15);
+    Print(head);
+
+}
+
+
+/*
+How many numbers?
+5
+Enter the number
+1
+List is : 1
+Enter the number
+2
+List is : 2 1
+Enter the number
+3
+List is : 3 2 1
+Enter the number
+4
+List is : 4 3 2 1
+Enter the number
+5
+List is : 5 4 3 2 1
+List is : 5 4 3 2 1 15
+
+*/
