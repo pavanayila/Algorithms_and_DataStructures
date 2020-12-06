e5be87a738886025ed8a0597d0e43467a8bb68b4
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-12-06 14:48:11 +0530

    Add files via upload

---
+++ 2) Doubly Linked lists/C/1) get_new_node/get_new_node1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+
+struct node *get_new_node(struct node *head, int data)  // Insertion in an empty list
+{
+    struct node *temp = malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+    head = temp;
+    return head;
+}
+
+void Print(struct node *head)
+{
+	struct node* temp = head;
+	while(temp != NULL)
+    {
+		printf("%d ",temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+    struct node *head;
+    int n;
+    head = NULL;
+
+    printf("Enter the data for node: ");
+    scanf("%d", &n);
+    head = get_new_node(head, n);
+    Print(head);
+
+}
+
+/*
+
+Enter the data for node: 5
+5
+
+*/
