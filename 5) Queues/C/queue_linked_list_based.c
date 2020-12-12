558532cf822ef7fc1a97a95c110da10d1dbaccd6
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-12-12 10:43:04 +0530

    Add files via upload

---
+++ 5) Queues/C/queue_linked_list_based.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+void Enqueue(int x)
+{
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+
+    if(front == NULL && rear == NULL)
+    {
+        front = rear = temp;
+        return;
+    }
+    rear->next = temp;
+    rear = temp;
+}
+
+void Dequeue()
+{
+    struct Node *temp = front;
+    if(front == NULL)
+    {
+        printf("Queue is Empty\n");
+        return;
+    }
+    if(front == rear)
+    {
+        front = rear = NULL;
+    }
+    else
+    {
+        front = front->next;
+    }
+    free(temp);
+}
+
+int Front()
+{
+    if(front == NULL)
+    {
+        printf("Queue is Empty\n");
+        return;
+    }
+    return front->data;
+}
+
+void Print()
+{
+    struct Node *temp = front;
+    while(temp != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    Enqueue(2); Print();
+	Enqueue(4); Print();
+	Enqueue(6); Print();
+	Dequeue();  Print();
+	Enqueue(8); Print();
+}
+
+/*
+2
+2 4
+2 4 6
+4 6
+4 6 8
+*/
