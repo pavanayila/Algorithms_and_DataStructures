92a2e0a24b73b84900672b984f7c107b0edc226e
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-12-25 10:33:26 +0530

    Add files via upload

---
+++ 6) Trees/C++/min_and_max_bst.cpp
@@ -0,0 +1,103 @@
+// Binary Search Tree - Implementation in C++
+#include<iostream>
+using namespace std;
+
+struct BstNode
+{
+    int data;
+    BstNode *left;
+    BstNode *right;
+};
+
+BstNode *GetNewNode(int data)
+{
+    BstNode *newNode = new BstNode();
+    newNode->data = data;   // (*newNode).data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+BstNode *Insert(BstNode *root, int data)
+{
+    if(root == NULL)
+    {
+        root = GetNewNode(data);
+        return root;
+    }
+    else if(data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+bool Search(BstNode *root, int data)
+{
+    if(root == NULL)
+    {
+        return false;
+    }
+    else if(root->data == data)
+    {
+        return true;
+    }
+    else if(data <= root->data)
+    {
+        return Search(root->left, data);
+    }
+    else
+    {
+        return Search(root->right, data);
+    }
+}
+
+int FindMin(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout<<"Error: Tree is empty\n";
+        return -1;
+    }
+    else if(root->left == NULL)
+    {
+        return root->data;       // (*root).data
+    }
+    // search in left subtree
+    return FindMin(root->left);  //
+}
+
+int FindMax(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout<<"Error: Tree is empty\n";
+        return -1;
+    }
+    else if(root->right == NULL)
+    {
+        return root->data;       // (*root).data
+    }
+    // search in right subtree
+    return FindMax(root->right);
+}
+int main()
+{
+    BstNode *root; // pointer to root node
+    root = NULL;   // setting tree as empty
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    cout<<"Minimun element is : "<<FindMin(root)<<endl;
+    cout<<"Maximum element is : "<<FindMax(root)<<endl;
+}
+
+/*
+Minimun element is : 8
+Maximum element is : 25
+*/
