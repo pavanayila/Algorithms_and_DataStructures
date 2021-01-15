2f40fce46dc66624a70e3ff1e28e61037633746b
pavanayila <67941868+pavanayila@users.noreply.github.com>
2021-01-15 17:30:06 +0530

    Add files via upload

---
+++ 1) Linked Lists/Python/add_at_beg.py
@@ -0,0 +1,49 @@
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def print_list(self):
+		cur_node = self.head
+		while cur_node:
+			print(cur_node.data)
+			cur_node = cur_node.next
+
+	def append(self, data):
+		new_node = Node(data)
+		if self.head is None:
+			self.head = new_node
+			return
+		last_node = self.head
+		while last_node.next:
+			last_node = last_node.next
+		last_node.next = new_node
+
+	def prepend(self, data):
+		new_node = Node(data)
+		new_node.next = self.head
+		self.head = new_node
+
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+llist.append(5)
+llist.append("A")
+llist.prepend("E")
+llist.print_list()
+
+"""
+E
+1
+2
+3
+4
+5
+A
+"""
